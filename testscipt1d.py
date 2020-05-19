96c79fb648ba885c59a643f18ea995fe510e435b
HuiWang97 <65461517+HuiWang97@users.noreply.github.com>
2020-05-19 22:34:11 +0200

    Update testscipt1d.py

--- testscipt1d.py
+++ testscipt1d.py
@@ -5,11 +5,14 @@ from seminar_03 import guided_modes_1DTE
 
 plt.close('all')
 
-grid_size     = ?? # please choose appropriate value
-number_points = ?? # please choose appropriate value
-h             = ?? # please choose appropriate value
+grid_size     = 300 # please choose appropriate value
+#number_points = 300       # please choose appropriate value
+h             = 0.05      # please choose appropriate value
 lam           = 0.78
 k0            = 2*np.pi/lam
 e_substrate   = 1.5**2
 delta_e       = 1.5e-2
 w             = 15.0
+
+prm = epsilon_gauss(grid_size, h , e_substrate, delta_e, w)
+eigval, eigvec=guided_modes_1DTE(prm, k0, h)
