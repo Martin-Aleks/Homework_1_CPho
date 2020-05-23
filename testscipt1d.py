44a7b4392d2d385b2cd21212a70f80cc4e4d595b
HuiWang97 <65461517+HuiWang97@users.noreply.github.com>
2020-05-23 20:28:03 +0200

    Update testscipt1d.py

--- testscipt1d.py
+++ testscipt1d.py
@@ -24,7 +24,23 @@ time_end = time.time()   #Timer ends
  
 time_c= time_end - time_start
 
-fig=plt.figure(figsize=(10,10))
-x = h*(np.linspace(1,grid_size,grid_size)-round(grid_size/2))
-plt.plot(x,eigvec)
+legend1=list()
+for val in eigval:
+    legend1.append("eigenvalue {:.5f}".format(val))
+    
+
+
+fig = plt.figure(figsize=[10,10]) 
+ax=fig.add_subplot(111)
+x=h*(np.linspace(1,grid_size,grid_size)-round(grid_size/2))
+ax.plot(x,eigvec)
+ax2 = ax.twinx()
+ax2.plot(x,prm,'--')
+ax.set_position([0.2, 0.1, 0.8, 0.8])
+ax.legend(legend1,loc='best')
+ax.set(xlabel = 'x/$\mu m$', ylabel = 'E/a.u.')
+ax2.set(ylabel='permittivity')
+plt.title('Eigenmodes')
+plt.tight_layout()
+plt.savefig('{0}_{1}_1D.png'.format(grid_size,h))
 plt.show()
