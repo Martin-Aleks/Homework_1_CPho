cf0d25c4846d893951e8f52c771f778ec4c0e46e
HuiWang97 <65461517+HuiWang97@users.noreply.github.com>
2020-05-23 21:55:04 +0200

    Update Homework_1_function_headers.py

--- Homework_1_function_headers.py
+++ Homework_1_function_headers.py
@@ -142,21 +142,76 @@ def guided_modes_2D(prm, k0, h, numb):
     """
     
     # Building the big matrix for N^2 by N^2 for an N by N grid of points
-    arr0 = np.ones(N**2)*(-4)
-    arr1 = np.ones((N**2)-1)
-    arr2 = np.ones(N*(N-1))
-    for i in range(0, (N-1)): 
-        arr1[(N-1)+(i*N)]=0
-    arr0 = arr0/(k0**2)/(h**2) + prm
-    arr1 = arr1/(k0**2)/(h**2)
-    arr2 = arr2/(k0**2)/(h**2)
-    data = (arr2, arr1, arr0, arr1, arr2)
-    M = sps.diags(data, [-N, -1, 0, 1, N])
+    H,W = prm.shape
+    prm = prm.flatten()
+    # Building the big matrix for h*w by h*w for an h by w grid of points
+    #Calculate the elements along 4 diagnal lines
+    arr0 = np.ones(H*W)*(-4) #
 
+    arr1 = np.ones((H*W)-1)
+
+    arr2 = np.ones(W*(H-1))
+
+    for i in range(0, (H-1)): 
+
+        arr1[(W-1)+(i*W)]=0
+
+    arr0 = arr0/(k0**2)/(h**2) + prm
+
+    arr1 = arr1/(k0**2)/(h**2)
+
+    arr2 = arr2/(k0**2)/(h**2)
+    #Construct the maxtrix M
+    data = (arr2, arr1, arr0, arr1, arr2)
+
+    M = sps.diags(data, [-W, -1, 0, 1, W])
+
+    #select the proper number of eigenvalues
+    N_cal = 0 #Number of eigenmodes that have been calculated
+    e_min = min(prm) #limitation of effective permittivity
+    e_max = max(prm) #limitation of effective permittivity
+    val_eff1 = np.zeros(numb,complex) #store the eigenvalues
+    vec_eff1 = np.zeros([H*W,numb],complex) #store the eigenvectors
     
-    vals, vecs = eigs(M, k=4, sigma = 2.25) # Needs a second look
-    
-    pass
+    while N_cal<numb: 
+        #when we still need to calculate more eigenvaluess that satisfy the limitation
+        #notice that lower order modes have larger effective permittivity
+        vals,vecs=eigs(M,k=2*numb,sigma=e_max) #calculate eigenvalues around e_max (the eigenvalues are arranged according to the value of abs(vals-sigma) from small to large )
+        eigval1=vals[(e_min<vals)&(vals<e_max)]  #then choose the modes that satisfy the limitation
+        eigvec1=vecs[:,(e_min<vals)&(vals<e_max)] 
+        if (eigval1.size!=0):
+            #when there are some modes that satisfy the limitation
+            if (numb-N_cal<=eigval1.size):
+                #if the calculation result provides enough number of modes we want
+                #choose the number of modes we need
+                val_eff1[N_cal:numb]=eigval1[0:(numb-N_cal)]
+                vec_eff1[:,N_cal:numb]=eigvec1[:,0:(numb-N_cal)]
+                N_cal=numb #update N_cal
+            elif (numb-N_cal>eigval1.size):
+                #if we still need to do more calculation
+                #first store the results we have got
+                val_eff1[N_cal:(N_cal+eigval1.size)]=eigval1[:]
+                vec_eff1[:,N_cal:(N_cal+eigval1.size)]=eigvec1[:,:]
+                N_cal=N_cal+eigval1.size #update N_cal to start next iteration
+                e_max=min(val_eff1) #update the e_max to make sure that we don't select the modes that have been calculated(larger eigenvalues have been calculated)
+        else:
+            #when all the eigenvalues that satisfy the limitation have be calculated
+            #but we didn't get enough number of modes we want
+            str='There are not enough allowed eigenmodes in the structure.'\
+            'The total number of modes that can exist is {0}'.format(N_cal)
+            warnings.warn(str)
+            break
+        
+    if N_cal==0:
+        #if there is no mode that satisfies the limitation, raise an error
+        raise ValueError('There exits no eigenmodes.')
+    else:
+        #reshape the size of eigenvectors
+        vec_eff1=vec_eff1[:,0:N_cal]
+        vec_eff1=vec_eff1.T
+        vec_eff1=vec_eff1.reshape(N_cal,H,W)
+
+    return val_eff1[0:N_cal],vec_eff1
 
 
 
